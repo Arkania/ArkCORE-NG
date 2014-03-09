06ed94b9068d8d5635c8b60537bd9f4fa4b38be2
lev76 <king.monstr@yandex.ru>
2014-03-09 18:17:43 -0400

    Update LFGScripts.cpp

--- src/server/game/DungeonFinding/LFGScripts.cpp
+++ src/server/game/DungeonFinding/LFGScripts.cpp
@@ -125,8 +125,9 @@ void LFGGroupScript::OnRemoveMember(Group* group, uint64 guid, RemoveMethod meth
     {
         if ((method == GROUP_REMOVEMETHOD_LEAVE && sLFGMgr->GetState(gguid) != LFG_STATE_FINISHED_DUNGEON && sLFGMgr->GetDungeon(gguid,false)))
             player->CastSpell(player, LFG_SPELL_DUNGEON_DESERTER, true);
-
         /*
+        if (method == GROUP_REMOVEMETHOD_LEAVE)
+            // Add deserter flag
         else if (group->isLfgKickActive())
             // Update internal kick cooldown of kicked
         */
