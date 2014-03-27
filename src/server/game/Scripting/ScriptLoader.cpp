6c16dc751f1bae188c0e662df4a97ec8ea4043bf
gpn39f <frankberg@foni.net>
2014-03-27 16:41:15 +0100

    merge trainings dummy from arkcore
    Signed-off-by: ladel <infobox12@mail.ru>

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -90,6 +90,8 @@ void AddSC_npc_innkeeper();
 void AddSC_npcs_special();
 void AddSC_npc_taxi();
 void AddSC_achievement_scripts();
+void AddSC_npc_trainings_dummy();
+
 
 //eastern kingdoms
 void AddSC_alterac_valley();                 //Alterac Valley
@@ -845,6 +847,7 @@ void AddWorldScripts()
     AddSC_npc_taxi();
     AddSC_achievement_scripts();
     AddSC_chat_log();
+	AddSC_npc_trainings_dummy();
 #endif
 }
 
