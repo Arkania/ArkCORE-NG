9229e48e5739f73989bec1e80de779ec359a2b78
Serge Winters <serge@arkania.net>
2014-03-29 20:02:26 -0600

    Core/Scripts: Placeholders for Events and Player Pets at Scripts folder. some FIXME at pet_hunter.ccp
    Signed-off-by: ladel <infobox12@mail.ru>

---
+++ src/server/scripts/Pet/pet_hunter.cpp
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2011-2014 ArkCORE <http://www.arkania.net/>  
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Ordered alphabetically using scriptname.
+ * Scriptnames of files in this file should be prefixed with "npc_pet_hun_".
+ */
+
+#include "ScriptMgr.h"
+#include "ScriptedCreature.h"
+#include "Common.h"
+#include "CreatureAIImpl.h"
+#include "Player.h"
+#include "Creature.h"
+#include "Spell.h"
+#include "CreatureAI.h"
+#include "Pet.h"
+#include "CreatureGroups.h"
+#include "PetAI.h"
+
+
+enum HunterSpells
+{
+    SPELL_HUNTER_CRIPPLING_POISON       = 30981, // Viper
+    SPELL_HUNTER_DEADLY_POISON_PASSIVE  = 34657, // Venomous Snake
+    SPELL_HUNTER_MIND_NUMBING_POISON    = 25810  // Viper
+};
+
+enum HunterCreatures
+{
+    NPC_HUNTER_VIPER                    = 19921
+};
+
+class npc_pet_hunter_snake_trap : public CreatureScript
+{
+    public:
+        npc_pet_hunter_snake_trap() : CreatureScript("npc_pet_hunter_snake_trap") { }
+
+        struct npc_pet_hunter_snake_trapAI : public ScriptedAI
+        {
+            npc_pet_hunter_snake_trapAI(Creature* creature) : ScriptedAI(creature) { }
+
+            void EnterCombat(Unit* /*who*/)  { }
+
+            void Reset() 
+            {
+                _spellTimer = 0;
+
+                CreatureTemplate const* Info = me->GetCreatureTemplate();
+
+                _isViper = Info->Entry == NPC_HUNTER_VIPER ? true : false;
+
+                me->SetMaxHealth(uint32(107 * (me->getLevel() - 40) * 0.025f));
+                // Add delta to make them not all hit the same time
+                uint32 delta = (rand() % 7) * 100;
+                me->SetAttackTime(BASE_ATTACK, Info->baseattacktime + delta);
+                //me->SetStatFloatValue(UNIT_FIELD_RANGED_ATTACK_POWER, float(Info->attackpower));
+
+                // Start attacking attacker of owner on first ai update after spawn - move in line of sight may choose better target
+                if (!me->GetVictim() && me->IsSummon())
+                    if (Unit* Owner = me->ToTempSummon()->GetSummoner())
+//uncomment  FIXME                      if (Owner->getAttackerForHelper())
+     //and this                       AttackStart(Owner->getAttackerForHelper());
+
+                if (!_isViper)
+                    DoCast(me, SPELL_HUNTER_DEADLY_POISON_PASSIVE, true);
+            }
+
+            // Redefined for random target selection:
+            void MoveInLineOfSight(Unit* who) 
+            {
+				// FIXME
+               /* if (!me->GetVictim() && me->CanCreatureAttack(who))
+                {
+                    if (me->GetDistanceZ(who) > CREATURE_Z_ATTACK_RANGE)
+                        return;
+
+                    float attackRadius = me->GetAttackDistance(who);
+                    if (me->IsWithinDistInMap(who, attackRadius) && me->IsWithinLOSInMap(who))
+                    {
+                        if (!(rand() % 5))
+                        {
+                            me->setAttackTimer(BASE_ATTACK, (rand() % 10) * 100);
+                            _spellTimer = (rand() % 10) * 100;
+                            AttackStart(who);
+                        }
+                    }
+                }*/
+            }
+
+            void UpdateAI(const uint32 diff) 
+            {
+                if (!UpdateVictim())
+                    return;
+
+                if (me->GetVictim()->HasBreakableByDamageCrowdControlAura(me))
+                {
+                    me->InterruptNonMeleeSpells(false);
+                    return;
+                }
+
+                // Viper
+                if (_isViper)
+                {
+                    if (_spellTimer <= diff)
+                    {
+                        if (urand(0, 2) == 0) // 33% chance to cast
+                            DoCastVictim(RAND(SPELL_HUNTER_MIND_NUMBING_POISON, SPELL_HUNTER_CRIPPLING_POISON));
+
+                        _spellTimer = 3000;
+                    }
+                    else
+                        _spellTimer -= diff;
+                }
+
+                DoMeleeAttackIfReady();
+            }
+
+        private:
+            bool _isViper;
+            uint32 _spellTimer;
+        };
+
+        CreatureAI* GetAI(Creature* creature) const 
+        {
+            return new npc_pet_hunter_snake_trapAI(creature);
+        }
+};
+
+void AddSC_hunter_pet_scripts()
+{
+    new npc_pet_hunter_snake_trap();
+}
---
+++ src/server/scripts/Pet/pet_priest.cpp
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2011-2014 ArkCORE <http://www.arkania.net/>  
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Ordered alphabetically using scriptname.
+ * Scriptnames of files in this file should be prefixed with "npc_pet_pri_".
+ */
+
+#include "ScriptMgr.h"
+#include "ScriptedCreature.h"
+#include "PassiveAI.h"
+#include "PetAI.h"
+
+enum PriestSpells
+{
+    SPELL_PRIEST_GLYPH_OF_SHADOWFIEND       = 58228,
+    SPELL_PRIEST_GLYPH_OF_SHADOWFIEND_MANA  = 58227,
+    SPELL_PRIEST_LIGHTWELL_CHARGES          = 59907
+};
+
+class npc_pet_pri_lightwell : public CreatureScript
+{
+    public:
+        npc_pet_pri_lightwell() : CreatureScript("npc_pet_pri_lightwell") { }
+
+        struct npc_pet_pri_lightwellAI : public PassiveAI
+        {
+            npc_pet_pri_lightwellAI(Creature* creature) : PassiveAI(creature)
+            {
+                DoCast(me, SPELL_PRIEST_LIGHTWELL_CHARGES, false);
+            }
+
+            void EnterEvadeMode() 
+            {
+                if (!me->IsAlive())
+                    return;
+
+                me->DeleteThreatList();
+                me->CombatStop(true);
+                me->ResetPlayerDamageReq();
+            }
+        };
+
+        CreatureAI* GetAI(Creature* creature) const 
+        {
+            return new npc_pet_pri_lightwellAI(creature);
+        }
+};
+
+/*######
+# npc_shadowfiend
+######*/
+
+enum Shadowfiend
+{
+    MANA_LEECH                       = 28305,
+    GLYPH_OF_SHADOWFIEND_MANA        = 58227,
+    GLYPH_OF_SHADOWFIEND             = 58228
+};
+
+class npc_shadowfiend : public CreatureScript
+{
+public:
+    npc_shadowfiend() : CreatureScript("npc_shadowfiend") { }
+
+    struct npc_shadowfiendAI : public ScriptedAI
+    {
+        npc_shadowfiendAI(Creature* creature) : ScriptedAI(creature) {}
+
+        void Reset() 
+        {
+            if (me->IsSummon())
+                if (Unit* owner = me->ToTempSummon()->GetSummoner())
+                    if (Unit* pet = owner->GetGuardianPet())
+                        pet->CastSpell(pet, MANA_LEECH, true);
+        }
+ 
+        void DamageTaken(Unit* /*killer*/, uint32& damage) 
+        {
+            if (me->IsSummon())
+                if (Unit* owner = me->ToTempSummon()->GetSummoner())
+                    if (owner->HasAura(GLYPH_OF_SHADOWFIEND) && damage >= me->GetHealth())
+                        owner->CastSpell(owner, GLYPH_OF_SHADOWFIEND_MANA, true);
+        }
+
+        void UpdateAI(uint32 /*diff*/) 
+        {
+            if (!UpdateVictim())
+                return;
+
+            DoMeleeAttackIfReady();
+        }
+    };
+
+    CreatureAI* GetAI(Creature* creature) const 
+    {
+        return new npc_shadowfiendAI(creature);
+    }
+};
+
+void AddSC_priest_pet_scripts()
+{
+    new npc_pet_pri_lightwell();
+	new npc_shadowfiend();
+}
