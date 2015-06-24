e94064c8f1e66bdf08b8ca4973069a143db4dbac
gpn39f <frankberg@foni.net>
2015-06-24 23:33:52 +0200

    update quest 12641

---
+++ sql/updates/world/2015_06_20_01_world.sql
@@ -0,0 +1,7 @@
+
+-- quest 12641 Death Comes From On High
+update creature_template set modelid1=23980, modelid2=11686 where entry in (28525, 28542, 28543, 28544);
+update creature_addon set auras=51939 where guid in (236953, 237079, 237083, 237087);
+
+
+
