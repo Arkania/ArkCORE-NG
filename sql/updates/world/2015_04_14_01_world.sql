c70f5a4aeee20b6fc3c075e9ddfbbff0bcc87777
gpn39f <frankberg@foni.net>
2015-06-24 21:20:48 +0200

    cleanup sql folder.. move old to old

--- sql/updates/world/2015_04_14_01_world.sql
+++
@@ -1,5 +0,0 @@
-
-update creature_template set AIName="", ScriptName="npc_spitescale_showfight" where entry in (38300, 38301, 38302);
-update creature_template set AIName="", ScriptName="npc_darkspear_showfight" where entry in (38324, 38326);
-
-
--- sql/updates/world/2015_04_16_01_world.sql
+++
@@ -1,5 +0,0 @@
-
-update quest_template set RequestItemsText="Did you find the Collector?  Did you discover whom he's working for?" where Id=147;
-
-
-
