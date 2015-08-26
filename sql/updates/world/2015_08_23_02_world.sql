1ca150e87a835478670be162aaa0a9b01cb1577b
gpn39f <frankberg@foni.net>
2015-08-26 17:58:26 +0200

    fixes some issues from dynzone

---
+++ sql/updates/world/2015_08_23_02_world.sql
@@ -0,0 +1,11 @@
+
+-- #19
+UPDATE quest_template SET RequiredRaces=2098253 WHERE Id=13188;
+
+-- #18
+UPDATE quest_template SET Method=2 WHERE Id=13166;
+
+-- #14
+update spell_area set quest_start_status=74 where spell=53081 and area=4298;
+
+
