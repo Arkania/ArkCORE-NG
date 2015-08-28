910ef531b30f24a2df5e55788c1f5a9c338d91fe
gpn39f <frankberg@foni.net>
2015-08-28 10:34:09 +0200

    change quest fishing to smaller area

--- sql/updates/world/2015_08_25_01_world.sql
+++ sql/updates/world/2015_08_25_01_world.sql
@@ -1,10 +1,10 @@
 
 -- fishing quest in stormwind 
-delete from fishing_loot_template where entry=1519 and item=58503;
+delete from fishing_loot_template where entry=5314 and item=58503;
 insert into fishing_loot_template values
-(1519, 58503, -25.1941, 1, 0, 1, 1);
+(5314, 58503, -25.1941, 1, 0, 1, 1);
 
 -- fishing quest in Elwynn Forest
-delete from fishing_loot_template where entry=12 and item=58899;
+delete from fishing_loot_template where entry=61 and item=58899;
 insert into fishing_loot_template values
-(12, 58899, -35.4636, 1, 0, 1, 1);
+(61, 58899, -35.4636, 1, 0, 1, 1);
