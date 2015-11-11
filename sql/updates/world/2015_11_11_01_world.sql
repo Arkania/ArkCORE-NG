14105f51a8e43249c4b4c2f9b72c1bd9dbd8f695
gpn39f <frankberg@foni.net>
2015-11-11 23:29:02 +0100

    fix quest 871
    thanks to mrelfire and roger

---
+++ sql/updates/world/2015_11_11_01_world.sql
@@ -0,0 +1,8 @@
+
+
+
+UPDATE locales_quest SET EndText_loc3="", CompletedText_loc3="Kehrt zu Kargal in der Fernwacht im Nördlichen Brachland zurück.", QuestGiverTextWindow_loc3="Stacheleber vom Klingenmäenstamm", QuestGiverTargetName_loc3="Stacheleber vom Klingenmäenstamm" WHERE Id=871;
+
+UPDATE quest_template SET RequiredNpcOrGo2=3265, RequiredNpcOrGo3=0, RequiredNpcOrGoCount2=3, RequiredNpcOrGoCount3=0 WHERE Id=871;
+
+
