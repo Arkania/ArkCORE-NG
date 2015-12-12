9f3f3360a90615743cf5bd3bb31b6ef2115f0151
gpn39f <frankberg@foni.net>
2015-12-12 21:53:15 +0100

    cleanup the update folder

--- sql/updates/world/2015_12_02_02_world.sql
+++
@@ -1,4 +0,0 @@
-
-update creature_template set AIName="", Scriptname="npc_fez_34543" where entry=34543;
-update creature_template set AIName="", Scriptname="" where entry=34544;
-
--- sql/updates/world/2015_12_08_02_world.sql
+++
@@ -1,5 +0,0 @@
-
--- banner is spawned from script
-delete from gameobject where guid=183493 and id=195148;
-
-
