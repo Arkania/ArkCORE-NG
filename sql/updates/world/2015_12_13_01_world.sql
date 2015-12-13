eed8f01144662947489c84156caf39b5ce320695
gpn39f <frankberg@foni.net>
2015-12-13 20:14:54 +0100

    fix damage values for npc 49869

---
+++ sql/updates/world/2015_12_13_01_world.sql
@@ -0,0 +1,5 @@
+
+-- https://cata-twinhead.twinstar.cz/?npc=49869
+UPDATE creature_template SET mindmg=24, maxdmg=50, attackpower=100, rangeattacktime=2000, minrangedmg=16, maxrangedmg=24 WHERE entry=49869;
+
+
