8d9552ba8c62eba0bdc33ce15caef32f85e5ed3e
gpn39f <frankberg@foni.net>
2015-12-20 18:14:20 +0100

    reduce more dmg values to regular value..

---
+++ sql/updates/world/2015_12_15_07_world.sql
@@ -0,0 +1,74 @@
+
+
+-- extreme damage for level 6-6,7-7,6-7
+Update creature_template SET mindmg=7, maxdmg=9, minrangedmg=4, maxrangedmg=4 WHERE entry in (1543, 1547, 1935, 34697, 35810, 36180, 36184, 36186, 36428, 36429, 36432, 36463, 36464, 36465, 36496, 36501, 36732, 38441, 38510, 38647, 38746, 42473, 48494, 48496);
+Update creature_template SET mindmg=9, maxdmg=12, minrangedmg=6, maxrangedmg=8 WHERE entry in (1535, 1934, 35837, 36129, 36153, 36615, 35894, 36422);
+Update creature_template SET mindmg=7, maxdmg=12, minrangedmg=4, maxrangedmg=8 WHERE entry in (35882, 36103, 36149, 36176, 36348, 36383, 36421, 36719, 39655);
+
+-- extreme damage for level 8-8, 7-8
+Update creature_template SET mindmg=10, maxdmg=14, minrangedmg=7, maxrangedmg=10 WHERE entry in (1537, 36063, 36104, 36112, 36423, 36424, 36578, 38224, 38696, 2, 44927);
+Update creature_template SET mindmg=9, maxdmg=14, minrangedmg=6, maxrangedmg=10 WHERE entry in (38811, 1548, 33420, 35904, 35928, 35929, 36042, 36293, 36681, 36813, 38187, 38359, 38575, 38753, 38845);
+
+-- extreme damage for level 9-9, 8-9
+Update creature_template SET mindmg=11, maxdmg=15, minrangedmg=8, maxrangedmg=11 WHERE entry in (1523, 38808, 38809, 38810, 1534, 1656, 1662, 35893, 35917, 36117, 36417, 36143);
+Update creature_template SET mindmg=10, maxdmg=15, minrangedmg=7, maxrangedmg=11 WHERE entry in (37757, 1530, 37492, 39042, 39049, 39064, 39068, 39069);
+
+-- extreme damage for level 10-10, 9-10
+Update creature_template SET mindmg=12, maxdmg=16, minrangedmg=8, maxrangedmg=12 WHERE entry in (38124, 35807, 35875, 36115, 36127, 36418, 36525, 37733, 37735, 38518, 38526, 39037, 39063, 41768, 38120, 38615);
+Update creature_template SET mindmg=11, maxdmg=16, minrangedmg=8, maxrangedmg=12 WHERE entry in (1555, 39193, 39354, 39376, 39449, 39591, 39044);
+
+-- extreme damage for level 11-11, 10-11
+Update creature_template SET mindmg=15, maxdmg=20, minrangedmg=9, maxrangedmg=14 WHERE entry in (39351, 12355, 39352, 39353);
+Update creature_template SET mindmg=12, maxdmg=20, minrangedmg=8, maxrangedmg=14 WHERE entry in (37785, 40064, 40067, 40068);
+
+-- extreme damage for level 12-12, 11-12
+Update creature_template SET mindmg=17, maxdmg=22, minrangedmg=11, maxrangedmg=17 WHERE entry in (590, 39067, 43747);
+Update creature_template SET mindmg=15, maxdmg=22, minrangedmg=9, maxrangedmg=17 WHERE entry in (39589);
+
+-- extreme damage for level 13-13, 12-13
+Update creature_template SET mindmg=18, maxdmg=24, minrangedmg=12, maxrangedmg=18 WHERE entry in (16323, 16301);
+Update creature_template SET mindmg=17, maxdmg=24, minrangedmg=11, maxrangedmg=18 WHERE entry in (6180);
+
+-- extreme damage for level 14-14, 13-14
+Update creature_template SET mindmg=20, maxdmg=27, minrangedmg=13, maxrangedmg=20 WHERE entry in (45280, 39341, 45335, 48441, 48442);
+Update creature_template SET mindmg=18, maxdmg=27, minrangedmg=12, maxrangedmg=20 WHERE entry in (594, 449, 45532);
+
+-- 15
+Update creature_template SET mindmg=22, maxdmg=29, minrangedmg=15, maxrangedmg=22 WHERE entry in (426, 16221, 18038, 39002, 45286, 39066, 39147, 48266, 43103);
+Update creature_template SET mindmg=20, maxdmg=29, minrangedmg=13, maxrangedmg=22 WHERE entry in (3425);
+
+-- 16
+Update creature_template SET mindmg=23, maxdmg=31, minrangedmg=16, maxrangedmg=23 WHERE entry in (428, 446, 711, 44635);
+Update creature_template SET mindmg=22, maxdmg=31, minrangedmg=15, maxrangedmg=23 WHERE entry in (580);
+
+-- 17
+Update creature_template SET mindmg=25, maxdmg=33, minrangedmg=17, maxrangedmg=25 WHERE entry in (4064, 437, 43710);
+
+-- 18
+Update creature_template SET mindmg=26, maxdmg=35, minrangedmg=18, maxrangedmg=26 WHERE entry in (36852);
+Update creature_template SET mindmg=25, maxdmg=35, minrangedmg=17, maxrangedmg=26 WHERE entry in (598, 6380);
+
+-- 19
+Update creature_template SET mindmg=27, maxdmg=37, minrangedmg=19, maxrangedmg=27 WHERE entry in (36660, 36680, 36687, 4464);
+
+-- 20 
+Update creature_template SET mindmg=29, maxdmg=39, minrangedmg=20, maxrangedmg=30 WHERE entry in (46890, 25891, 50561);
+Update creature_template SET mindmg=27, maxdmg=39, minrangedmg=19, maxrangedmg=30 WHERE entry in (435, 41080, 42107);
+
+-- 25
+Update creature_template SET mindmg=39, maxdmg=53, minrangedmg=24, maxrangedmg=36 WHERE entry in (16541, 17549, 25888, 24935);
+Update creature_template SET mindmg=35, maxdmg=53, minrangedmg=23, maxrangedmg=30 WHERE entry in (3774);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
