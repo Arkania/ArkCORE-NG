75c9fc37daa3cd0c9836553793e1d813c23c2b58
gpn39f <frankberg@foni.net>
2015-12-25 03:41:39 +0100

    import creature_text for gilneas 38415

---
+++ sql/updates/world/2015_12_17_00_world.sql
@@ -0,0 +1,8 @@
+
+delete from creature_text where entry=38415;
+insert into creature_text values
+(38415, 0, 0, "Gilneas will remember your courage forever, Liam.", 12, 0, 100, 0, 1, 0, "say",50477),
+(38415, 1, 0, "Let us join your father's force's, Liam.  They'll need our help against Sylvanas.", 14, 0, 100, 0, 1, 0, "say",38361),
+(38415, 2, 0, "He's too strong!  Use the catapults to bring him down!", 14, 0, 100, 0, 1, 0, "say",38662);
+
+
