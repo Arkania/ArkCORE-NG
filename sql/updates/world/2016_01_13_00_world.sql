3a99c385a193ea647ea02656700c1a9a4ab51c80
gpn39f <frankberg@foni.net>
2016-01-14 08:37:51 +0100

    fix console errors

---
+++ sql/updates/world/2016_01_13_00_world.sql
@@ -0,0 +1,9 @@
+
+-- console error
+DELETE FROM creature_addon WHERE guid=152069;
+
+-- achievement removed befor client 4.03.. now observer 10569 is used..
+DELETE FROM achievement_criteria_data WHERE criteria_id=10570;
+DELETE FROM achievement_criteria_data WHERE criteria_id=10568;
+
+
