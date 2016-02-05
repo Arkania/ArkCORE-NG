d787eac969e02136783dc9c35ea4de22fe02ae53
gpn39f <frankberg@foni.net>
2016-02-05 21:02:30 +0100

    db part for druide spell wild mushroom

---
+++ sql/updates/world/2016_02_05_01_world.sql
@@ -0,0 +1,5 @@
+
+-- spell wild mushroom
+update creature_template set AIName="", ScriptName="npc_mushroom_47649" where entry = 47649;
+update creature_template set AIName="", ScriptName="npc_mushroom_43497" where entry = 43497;
+
