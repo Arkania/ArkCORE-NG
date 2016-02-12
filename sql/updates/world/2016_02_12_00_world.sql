813b7c179db0822f28001c855c7105200a348224
gpn39f <frankberg@foni.net>
2016-02-12 20:28:05 +0100

    fix issue #73
    adarrah has quest invisible aura 5 and the spell_aura was not set correctly for the player..

---
+++ sql/updates/world/2016_02_12_00_world.sql
@@ -0,0 +1,33 @@
+
+
+DELETE FROM spell_area WHERE spell=86749 AND area=980 AND quest_start=28557;
+INSERT INTO spell_area (spell, area, quest_start, quest_end, aura_spell, racemask, gender, autocast, quest_start_status, quest_end_status) VALUES 
+(86749, 980, 28557, 27922, 0, 946, 2, 1, 74, 11);
+
+DELETE FROM spell_area WHERE spell=86749 AND area=980 AND quest_start=28558;
+INSERT INTO spell_area (spell, area, quest_start, quest_end, aura_spell, racemask, gender, autocast, quest_start_status, quest_end_status) VALUES 
+(86749, 980, 28558, 27922, 0, 2098253, 2, 1, 74, 11);
+
+DELETE FROM spell_area WHERE spell=86749 AND area=980 AND quest_start=28295;
+INSERT INTO spell_area (spell, area, quest_start, quest_end, aura_spell, racemask, gender, autocast, quest_start_status, quest_end_status) VALUES 
+(86749, 980, 28295, 27922, 0, 2098253, 2, 1, 74, 11);
+
+DELETE FROM spell_area WHERE spell=86749 AND area=980 AND quest_start=28296;
+INSERT INTO spell_area (spell, area, quest_start, quest_end, aura_spell, racemask, gender, autocast, quest_start_status, quest_end_status) VALUES 
+(86749, 980, 28296, 27922, 0, 946, 2, 1, 74, 11);
+
+DELETE FROM spell_area WHERE spell=89521 AND area=980 AND quest_start=28557;
+INSERT INTO spell_area (spell, area, quest_start, quest_end, aura_spell, racemask, gender, autocast, quest_start_status, quest_end_status) VALUES 
+(89521, 980, 28557, 27922, 0, 946, 2, 1, 74, 11);
+
+DELETE FROM spell_area WHERE spell=89521 AND area=980 AND quest_start=28558;
+INSERT INTO spell_area (spell, area, quest_start, quest_end, aura_spell, racemask, gender, autocast, quest_start_status, quest_end_status) VALUES 
+(89521, 980, 28558, 27922, 0, 2098253, 2, 1, 74, 11);
+
+DELETE FROM spell_area WHERE spell=89521 AND area=980 AND quest_start=28295;
+INSERT INTO spell_area (spell, area, quest_start, quest_end, aura_spell, racemask, gender, autocast, quest_start_status, quest_end_status) VALUES 
+(89521, 980, 28295, 27922, 0, 2098253, 2, 1, 74, 11);
+
+DELETE FROM spell_area WHERE spell=89521 AND area=980 AND quest_start=28296;
+INSERT INTO spell_area (spell, area, quest_start, quest_end, aura_spell, racemask, gender, autocast, quest_start_status, quest_end_status) VALUES 
+(89521, 980, 28296, 27922, 0, 946, 2, 1, 74, 11);
