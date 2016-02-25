6e7f5d0284e8f1f0cf825c607471572bd2c7a496
gpn39f <frankberg@foni.net>
2016-02-25 11:41:02 +0100

    fix issue #85

---
+++ sql/updates/world/2016_02_24_01_world.sql
@@ -0,0 +1,13 @@
+
+-- fix issue #85
+
+update creature_template set gossip_menu_id=4822 where entry=2485;
+
+update creature_template set gossip_menu_id=4826 where entry=5958;
+
+update creature_template set gossip_menu_id=9581 where entry=27703;
+
+update creature_template set gossip_menu_id=11972 where entry=45139;
+
+update creature_template set gossip_menu_id=12235 where entry=47253;
+
