9d3030153152983003a4b2c4d487da6209ddf1cc
gpn39f <frankberg@foni.net>
2016-05-08 10:46:35 +0200

    cleanup update folder for 2016/04

--- sql/updates/world/2016_04_13_01_world.sql
+++
@@ -1,7 +0,0 @@
-
-
--- fix issue #130
-
-delete from creature where id=35627 and position_z < 29.0;
-
-
--- sql/updates/world/2016_04_18_00_world.sql
+++
@@ -1,3 +0,0 @@
-
--- fix issue #98
-update creature_template set minlevel=40, maxlevel=41 where entry=10940;
