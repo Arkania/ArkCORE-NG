e3159456cfa3543f5b0b405d21de704b4e9ce0ac
gpn39f <frankberg@foni.net>
2016-08-16 18:10:04 +0200

    folder cleanup for update files from 2016 July

--- sql/updates/world/2016_07_18_00_world.sql
+++
@@ -1,5 +0,0 @@
-
-delete from spell_script_names where spell_id=72116;
-insert into spell_script_names values 
-(72116, "spell_shoot_liam_72116");
-
--- sql/updates/world/2016_07_22_01_world.sql
+++
@@ -1,5 +0,0 @@
-
-delete from spell_script_names where spell_id=72247;
-insert into spell_script_names values 
-(72247, "spell_iron_bomb_72247");
-
