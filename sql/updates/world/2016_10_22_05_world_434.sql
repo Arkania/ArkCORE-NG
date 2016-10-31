289db80d6f987f66ffd4cf7f8bd33b08af9afe87
gpn39f <frankberg@foni.net>
2016-10-31 11:34:11 +0100

    cleanup october 2016

--- sql/updates/world/2016_10_22_05_world_434.sql
+++
@@ -1,5 +0,0 @@
-
-ALTER TABLE `creature`
-ADD COLUMN `phaseGroup`  smallint(5) UNSIGNED NOT NULL DEFAULT 0 AFTER `phaseIds`;
-
-
--- sql/updates/world/2016_10_22_06_world_434.sql
+++
@@ -1,4 +0,0 @@
-
-ALTER TABLE `gameobject`
-ADD COLUMN `phaseGroup`  smallint(5) UNSIGNED NOT NULL DEFAULT 0 AFTER `phaseIds`;
-
