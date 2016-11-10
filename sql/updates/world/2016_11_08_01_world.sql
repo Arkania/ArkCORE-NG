d97cce374f6800614e5ec73a575bbc3c5d7b9201
gpn39f <frankberg@foni.net>
2016-11-10 23:01:51 +0100

    prepare more for lost island..

---
+++ sql/updates/world/2016_11_08_01_world.sql
@@ -0,0 +1,235 @@
+
+
+DELETE FROM creature_text WHERE entry=38432;
+INSERT INTO creature_text VALUES 
+(38432, 0, 0, "You hear that, naga? $N is on $G his : her; way to settle our \"negotiations\"!", 14, 0, 100, 15, 0, 0, "Megs Dreadshredder", 49938),
+(38432, 1, 0, "Everybody loves a pool pony. It's our fastest selling toy behind the Kablooey Bombs: Infant Edition.", 12, 0, 100, 0, 0, 0, "Megs Dreadshredder", 49951),
+(38432, 2, 0, "That pygmy helm should fool them, at least for a little while. Get back to town!", 12, 0, 100, 0, 0, 0, "Megs Dreadshredder", 44571);
+
+DELETE FROM creature_text WHERE entry=38455;
+INSERT INTO creature_text VALUES 
+(38455, 0, 0, "I got these little hellions on a short leash, $N. The naga won't attack us while we have their hatchlings.", 12, 0, 100, 1, 0, 0, "", 38356),
+(38455, 1, 0, "You ready to make their leader surrender? Ok, here we go!", 12, 0, 100, 1, 0, 0, "Ace Say", 38357),
+(38455, 2, 0, "Prepare to surrender to the Bilgewater Cartel!", 14, 0, 100, 5, 0, 0, "Ace Yell", 38376),
+(38455, 3, 0, "We're planning on having a special Naga appreciation sale after your surrender is all wrapped up. Thirty percent off most goods we have on stock, Red Light savings on Kaldorei artifacts today only!", 14, 0, 100, 5, 0, 0, "Ace Yell", 38379),
+(38455, 4, 0, "Ok, Naga leader, come on out of hiding and surrender in the name of the Bilgewater Cartel!", 14, 0, 100, 5, 0, 0, "Ace Yell", 38381),
+(38455, 5, 0, "Uhhh...dude, this does not look good. I'm out of here!", 12, 0, 100, 5, 0, 0, "Ace Say", 38358);
+
+DELETE FROM creature_text WHERE entry=38448;
+INSERT INTO creature_text VALUES 
+(38448, 0, 0, "WHO DARES?! You shall be destroyed!", 14, 0, 100, 15, 0, 0, "", 976),
+(38448, 1, 0, "WHO DARES?!", 14, 0, 100, 15, 0, 15140, "Faceless of the Deep", 38338),
+(38448, 2, 0, "Little goblins? I remember when your race was created.", 14, 0, 100, 1, 0, 15139, "Faceless of the Deep Yell", 38346),
+(38448, 3, 0, "I care nothing for your threaths or these Naga. Prepare to be removed from existence.", 14, 0, 100, 25, 0, 15140, "Faceless of the Deep Yell", 38349),
+(38448, 4, 0, "Now, $N, you will die.", 14, 0, 100, 0, 0, 15140, "Faceless of the Deep Yell", 38359);
+
+DELETE FROM creature_text WHERE entry=38387;
+INSERT INTO creature_text VALUES 
+(38387, 0, 0, "Get into the Ultimate Footbomb Uniform and get down to the docks. Help Thrall kick the Trade Prince's butt!", 12, 0, 100, 0, 0, 0, "Sassy Hardwrench", 43948),
+(38387, 1, 0, "$N, hop into one of those B.C. Eliminators and wipe out the pygmies!", 14, 0, 100, 25, 0, 0, "Sassy Hardwrench", 49973),
+(38387, 2, 0, "You're going to have to take the fight to them, $N. Look for Izzy just outside their village.", 12, 0, 100, 0, 0, 0, "Sassy Hardwrench", 49974),
+(38387, 3, 0, "Those zombies are slow. You don't have to run right through them. Don't let them eat your brains, $N!", 12, 0, 100, 0, 0, 0, "Sassy Hardwrench", 50013),
+(38387, 4, 0, "Goblin grenades versus gnomish land mines. Show them who's better, $N!", 12, 0, 100, 0, 0, 0, "Sassy Hardwrench", 50095),
+(38387, 5, 0, "I forget... did you take flying lessons back on Kezan?", 12, 0, 100, 0, 0, 0, "Sassy Hardwrench", 50097),
+(38387, 6, 0, "Better hurry up, $N. I think that Thrall has news on where our people are being held.", 12, 0, 100, 0, 0, 0, "Sassy Hardwrench", 50099),
+(38387, 7, 0, "First she left you for Chip, and now the Trade Prince! She was never good enough for you, $N.", 12, 0, 100, 0, 0, 0, "Sassy Hardwrench", 50114);
+
+DELETE FROM creature_text WHERE entry=38696;
+INSERT INTO creature_text VALUES 
+(38696, 0, 0, "Killum gobins! Na cayno da BOOM!", 14, 0, 100, 0, 0, 0, "Yngwie", 38553),
+(38696, 1, 0, "Gobin ookum ta z'mbee!", 14, 0, 100, 0, 0, 0, "", 38554),
+(38696, 2, 0, "Echuta, gobin!", 14, 0, 100, 15, 0, 0, "Yngwie", 38555),
+(38696, 3, 0, "Tay'kn gobins!", 14, 0, 100, 15, 0, 0, "Yngwie", 38560);
+
+DELETE FROM creature_text WHERE entry=38643;
+INSERT INTO creature_text VALUES 
+(38643, 0, 0, "I'm free! I owe you my life!", 12, 0, 100, 4, 0, 0, "Goblin Captive", 38565),
+(38643, 1, 0, "I thought I was a goner for sure!", 12, 0, 100, 1, 0, 0, "Goblin Captive", 38566),
+(38643, 2, 0, "I felt my life slipping out of me.", 12, 0, 100, 1, 0, 0, "Goblin Captive", 38567),
+(38643, 3, 0, "They were trying to change me into a zombie!!!", 12, 0, 100, 5, 0, 0, "Goblin Captive", 38568),
+(38643, 4, 0, "I gotta get out of here before they catch me again!", 12, 0, 50, 5, 0, 0, "Goblin Captive Say", 38569),
+(38643, 5, 0, "I really can't thank you enough!", 12, 0, 100, 18, 0, 0, "Goblin Captive", 38570),
+(38643, 6, 0, "They took the others up into the volcano. Some to turn into zombies, some to sacrifice to their turtle god, Volcanoth!", 12, 0, 100, 5, 0, 0, "Goblin Captive", 38571),
+(38643, 7, 0, "The other captives are up in the volcano... they're being sacrificed, or worse, zombiefied!", 12, 0, 100, 5, 0, 0, "Goblin Captive", 38572);
+
+
+DELETE FROM creature_text WHERE entry=38647;
+INSERT INTO creature_text VALUES 
+(38647, 0, 0, "$N, you gotta get in there and rescue our people!", 12, 0, 100, 5, 0, 0, "Izzy", 50007),
+(38647, 1, 0, "Goblin zombies? The Town-In-A-Box has to be warned!", 12, 0, 100, 0, 0, 0, "Izzy", 50008),
+(38647, 2, 0, "Someone needs to do something about that giant turtle in the cave!", 14, 0, 100, 0, 0, 0, "Izzy", 38780);
+
+DELETE FROM creature_text WHERE entry=38409;
+INSERT INTO creature_text VALUES 
+(38409, 0, 0, "Tauren Paladins!", 14, 0, 100, 0, 0, 0, "Goblin Survivor", 37329),
+(38409, 1, 0, "Headshoes!", 14, 0, 100, 0, 0, 0, "Goblin Survivor", 37332),
+(38409, 2, 0, "So I have this idea for a movie. Three gnomes find a bracelet of power...", 14, 0, 100, 0, 0, 0, "Goblin Survivor", 37334),
+(38409, 3, 0, "It's like bungie-jumping, right? But without a cord, see? And you start at the bottom. I guess it's just sorta like regular jumping. But with guns.", 14, 0, 100, 0, 0, 0, "Goblin Survivor", 37335),
+(38409, 4, 0, "Feed pigs rubber, 'til they bounce. There's got to be an application for that.", 14, 0, 100, 0, 0, 0, "Goblin Survivor", 37343),
+(38409, 5, 0, "A binary number system. It uses only two numbers. 6 and 11.", 14, 0, 100, 0, 0, 0, "Goblin Survivor", 37358),
+(38409, 6, 0, "Stairways... for horizontal surfaces!", 14, 0, 100, 0, 0, 0, "Goblin Survivor", 37365),
+(38409, 7, 0, "Fake food! Created by grinding up real food and pressing it into shapes of different food. With artificial flavoring!", 14, 0, 100, 0, 0, 0, "Goblin Survivor", 37413),
+(38409, 8, 0, "Water-proof soap!  For underwater use!", 14, 0, 100, 0, 0, 0, "Goblin Survivor", 37421),
+(38409, 9, 0, "Handheld one-way mirrors!", 14, 0, 100, 0, 0, 0, "Goblin Survivor", 37422),
+(38409, 10, 0, "Don't worry. The zombies are looking for brains. You're safe.", 14, 0, 100, 0, 0, 0, "Goblin Survivor", 38648),
+(38409, 11, 0, "Smoker!", 14, 0, 100, 274, 0, 0, "", 38649),
+(38409, 12, 0, "Little help here!", 14, 0, 100, 0, 0, 0, "Goblin Survivor", 38767);
+ 
+DELETE FROM creature_text WHERE entry=38738 AND groupid=2 AND id=0;
+INSERT INTO creature_text VALUES 
+(38738, 2, 0, "You're going to love this!", 12, 0, 100, 0, 0, 0, "Coach Crosscheck", 50083);
+
+DELETE FROM creature_text WHERE entry=38738 AND groupid=3 AND id=0;
+INSERT INTO creature_text VALUES 
+(38738, 3, 0, "We're gonna make you the ultimate footbomb uniform, kid. But first, we need parts. Lots of them.", 12, 0, 100, 0, 0, 0, "Coach Crosscheck", 50106);
+
+DELETE FROM creature_text WHERE entry=36471 AND groupid=1 AND id=0;
+INSERT INTO creature_text VALUES 
+(36471, 1, 0, "Remember, look for the pygmy leaders: Gaahl, Malmo, and Teloch.", 12, 0, 100, 0, 0, 0, "Foreman Dampwick", 50081);
+
+DELETE FROM creature_text WHERE entry=38124;
+INSERT INTO creature_text VALUES 
+(38124, 0, 0, "Genius idea, sir. Your brain power never ceases to amaze me!", 14, 0, 100, 1, 0, 0, "Assistant Greely", 38119),
+(38124, 1, 0, "This thing is huge! Reverse the polarity on the Poultryizer. We'll have to shove it in through the outtake.", 14, 0, 100, 5, 0, 0, "Assistant Greely", 38141),
+(38124, 2, 0, "There it goes.  It should only be a moment now....", 14, 0, 100, 1, 0, 0, "Assistant Greely", 38142),
+(38124, 3, 0, "$N, get to the Mechashark X-Steam Controller.  Rid us of the Hammer menace!", 12, 0, 100, 1, 0, 0, "Say Text on Quest Accept", 49911),
+(38124, 4, 0, "Look for the mounds of glowing stuff on the ground.", 12, 0, 100, 0, 0, 0, "Assistant Greely", 50082),
+(38124, 5, 0, "Greely drools a little as she continues her mindless labor.", 16, 0, 100, 0, 0, 0, "Assistant Greely", 50101),
+(38124, 6, 0, "Get rid of those Steamwheedle Sharks before they trounce us!", 12, 0, 100, 0, 0, 0, "Assistant Greely", 50105),
+(38124, 7, 0, "It's time for a little sabotage!", 12, 0, 100, 0, 0, 0, "Assistant Greely", 50109),
+(38124, 8, 0, "Deliver the footbomb uniform to the Slave Pits. Hurry, $N!", 12, 0, 100, 0, 0, 0, "Assistant Greely", 50111);
+
+
+DELETE FROM creature_text WHERE entry=38120;
+INSERT INTO creature_text VALUES 
+(38120, 0, 0, "Greely, load The Biggest Egg Ever into the Poultryizer. Mechnuggets for everyone!", 14, 0, 100, 0, 0, 0, "Hobart Grapplehammer", 38106),
+(38120, 1, 0, "Shut it down, shut it down! The eggs are bad!!!", 14, 0, 100, 0, 0, 0, "Hobart Grapplehammer", 38170),
+(38120, 2, 0, "$N, over here if you please.", 12, 0, 100, 3, 0, 0, "Hobart Grapplehammer", 41262),
+(38120, 3, 0, "It's really quite simple, $N. Just attach these fireworks to the wild cluckers and I'll use this controller to fly them into the cage.", 12, 0, 100, 5, 0, 0, "Hobart Grapplehammer", 49894),
+(38120, 4, 0, "Don't underestimate the Mechachicken, $N. After all, it is a Hobart Grapplehammer invention!", 12, 0, 100, 0, 0, 0, "Hobart Grapplehammer", 49900),
+(38120, 5, 0, "The marketing research Megs has conducted suggests that a naga invasion is imminent!", 12, 0, 100, 5, 0, 0, "Hobart Grapplehammer", 49934),
+(38120, 6, 0, "Take your time, my $G boy : girl;. Now what can I modify this shredder boot to be? Hmm.", 12, 0, 100, 0, 0, 0, "Hobart Grapplehammer", 50086),
+(38120, 7, 0, "I've set the Bootzooka to fire several rockets per shot. I'm fairly certain that I perfectly calibrated the targeting.", 12, 0, 100, 0, 0, 0, "Hobart Grapplehammer", 50087),
+(38120, 8, 0, "Booster rockets for cages. What will I think of next? I am a genius!", 12, 0, 100, 0, 0, 0, "Hobart Grapplehammer", 50112);
+
+DELETE FROM creature_text WHERE entry=38746;
+INSERT INTO creature_text VALUES 
+(38746, 1, 0, "Doc! Help!", 12, 0, 100, 0, 0, 0, "Gobber", 38772);
+
+DELETE FROM creature_text WHERE entry=36615;
+INSERT INTO creature_text VALUES 
+(36615, 2, 0, "On my way!", 14, 0, 100, 0, 0, 0, "Doc Zapnozzle", 38775),
+(36615, 3, 0, "Just a sec!", 12, 0, 100, 0, 0, 0, "Doc Zapnozzle", 38776),
+(36615, 0, 0, "I only have two hands and two feet here!", 14, 0, 100, 0, 0, 0, "Doc Zapnozzle", 38777),
+(36615, 4, 0, "I hope I don't run out of bandages!", 12, 0, 100, 0, 0, 0, "Doc Zapnozzle", 38778),
+(36615, 5, 0, "I need an assistant!", 12, 0, 100, 0, 0, 0, "Doc Zapnozzle", 38779),
+(36615, 1, 0, "Someone needs to do something about that giant turtle in the cave!", 14, 0, 100, 0, 0, 0, "Doc Zapnozzle", 38780);
+
+DELETE FROM creature_text WHERE entry=38928;
+INSERT INTO creature_text VALUES 
+(38928, 0, 0, "We have no time to lose! Hop on and let's get out of here!", 12, 0, 100, 0, 0, 0, "Sassy Hardwrench", 0),
+(38928, 1, 0, "$N, come on! Get over here!!!", 14, 0, 100, 0, 0, 0, "Sassy Hardwrench", 38908);
+
+DELETE FROM creature_text WHERE entry=38869;
+INSERT INTO creature_text VALUES 
+(38869, 0, 0, "Hang on, $N! We have to get out of here, NOW!", 14, 0, 100, 0, 0, 0, "", 38874),
+(38869, 1, 0, "OH MY GOSH! BEHIND YOU!!!", 14, 0, 100, 0, 0, 0, "Sassy Hardwrench", 38943),
+(38869, 2, 0, "WE'RE ON FIRE!", 14, 0, 100, 0, 0, 0, "Sassy Hardwrench", 38955),
+(38869, 3, 0, "Get in the back seat. We're getting out of here. I know where our old friends, the orcs, are!", 14, 0, 100, 0, 0, 0, "Sassy Hardwrench", 38973);
+
+DELETE FROM creature_text WHERE entry=39065;
+INSERT INTO creature_text VALUES 
+(39065, 0, 0, "Deliver justice for our dead, $N.", 12, 0, 100, 5, 0, 0, "Aggra", 50094),
+(39065, 1, 0, "You honor us with your actions on behalf of our dead, $N.", 12, 0, 100, 0, 0, 0, "Aggra", 50096),
+(39065, 2, 0, "$N hurry! Thrall needs your help!", 14, 0, 100, 0, 0, 0, "Aggra", 39527);
+
+DELETE FROM creature_text WHERE entry=39067;
+INSERT INTO creature_text VALUES 
+(39067, 1, 0, "Why are you playing with them? It's cruel.", 14, 1, 100, 11, 0, 0, "Scout Brax", 39101);
+
+DELETE FROM creature_text WHERE entry=39074;
+INSERT INTO creature_text VALUES 
+(39074, 1, 0, "Get back into the skies on the Tranquil Coast or be ejected!", 42, 0, 100, 0, 0, 0, "Pride of Kezan", 39116);
+
+DELETE FROM creature_text WHERE entry=39066;
+INSERT INTO creature_text VALUES 
+(39066, 0, 0, "Teach these cowards the true meaning of war!", 14, 0, 100, 397, 0, 0, "Kilag Gorefang", 39097),
+(39066, 1, 0, "Up the ramp, $N! Help Thrall defeat the Trade Prince!", 14, 0, 100, 0, 0, 0, "Kilag Gorefang", 39532),
+(39066, 2, 0, "Run, Bastia! Get $N to Sky Falls and the labor mine.", 12, 0, 100, 0, 0, 0, "Kilag Gorefang", 50100);
+
+DELETE FROM creature_text WHERE entry=38517;
+INSERT INTO creature_text VALUES 
+(38517, 0, 0, "Okay. We're going to move away from the exploding volcano and try to hook up with the orcs for the final assault on the Trade Prince. See you on the other side... I hope.", 12, 0, 100, 1, 0, 0, "Slinky Sharpshiv", 39240);
+
+DELETE FROM creature_text WHERE entry=39193;
+INSERT INTO creature_text VALUES 
+(39193, 0, 0, "Back to work!", 12, 0, 100, 0, 0, 0, "Brute Overseer", 39314),
+(39193, 0, 1, "You no slave! Work or die!", 12, 0, 100, 0, 0, 0, "Brute Overseer", 39315),
+(39193, 0, 2, "Me get big bonus for capture you!", 14, 0, 33, 0, 0, 0, "Brute Overseer", 39316),
+(39193, 0, 3, "Trade Prince say hurt you!", 14, 0, 33, 0, 0, 0, "Brute Overseer", 39318),
+(39193, 0, 4, "You no mine ore, you get beating!", 14, 0, 33, 0, 0, 0, "Brute Overseer", 39319),
+(39193, 0, 5, "Me like beat you up!", 12, 0, 100, 0, 0, 0, "Brute Overseer", 39320),
+(39193, 0, 6, "Me overseer, you slave!", 12, 0, 100, 0, 0, 0, "Brute Overseer", 39321),
+(39193, 0, 7, "Break time over!", 12, 0, 100, 0, 0, 0, "Brute Overseer", 39322);
+
+DELETE FROM creature_text WHERE entry=39199;
+INSERT INTO creature_text VALUES 
+(39199, 0, 0, "Wait, I have an idea!  Here, throw my Light-blessed shield at the beast!", 14, 0, 100, 0, 0, 0, "", 38689),
+(39199, 1, 0, "I'VE GOT AN IDEA!", 14, 0, 100, 0, 0, 0, "Assistant Greely", 39259),
+(39199, 2, 0, "We're not a part of your system!", 14, 0, 100, 0, 0, 0, "Assistant Greely", 39312),
+(39199, 3, 0, "We've got to get our people out of here, $N! The key is to get them to drink plenty of Kaja'Cola Zero-One!", 12, 0, 100, 0, 0, 0, "Assistant Greely", 50102),
+(39199, 4, 0, "$N, we need to escape through the northern tunnel!", 12, 0, 100, 0, 0, 0, "Assistant Greely", 50103);
+
+DELETE FROM creature_text WHERE entry=38745;
+INSERT INTO creature_text VALUES 
+(38745, 1, 0, "State-sponsored healthcare!", 14, 0, 100, 0, 0, 0, "Kezan Citizen", 37346),
+(38745, 0, 0, "Don't worry. The zombies are looking for brains. You're safe.", 14, 0, 100, 467, 0, 0, "Kezan Citizen", 38648),
+(38745, 2, 0, "Stairways... for horizontal surfaces!", 14, 0, 100, 0, 0, 0, "Kezan Citizen", 37365);
+
+DELETE FROM creature_text WHERE entry=39201;
+INSERT INTO creature_text VALUES 
+(39201, 0, 0, "Invisible condiments. For an unobstructed view of your food.", 14, 0, 100, 0, 0, 0, "Gobber", 37360),
+(39201, 1, 0, "Soap on a rope! Attached to a chain. Mounted via a steel cable. To some soap.", 14, 0, 100, 0, 0, 0, "Gobber", 37362),
+(39201, 2, 0, "Fake food! Created by grinding up real food and pressing it into shapes of different food. With artificial flavoring!", 14, 0, 100, 0, 0, 0, "Gobber", 37413);
+
+DELETE FROM creature_text WHERE entry=39200;
+INSERT INTO creature_text VALUES 
+(39200, 0, 0, "Leashes... with pre-attached pets.", 14, 0, 100, 0, 0, 0, "Izzy", 37327),
+(39200, 1, 0, "Shirts with leg-holes. Reversable shirtshorts!", 14, 0, 100, 0, 0, 0, "Izzy", 37328),
+(39200, 2, 0, "A globe that shows the inside of the planet... on the outside. So you know where to dig!", 14, 0, 100, 0, 0, 0, "Izzy", 37364);
+
+DELETE FROM creature_text WHERE entry=39198;
+INSERT INTO creature_text VALUES 
+(39198, 0, 0, "Handheld one-way mirrors!", 14, 0, 100, 0, 0, 0, "Ace", 37422),
+(39198, 1, 0, "Explosive fire extinguishers!  We'll fight fire... WITH fire!", 14, 0, 100, 0, 0, 0, "Ace", 37425),
+(39198, 2, 0, "Gloves with built-in clappers, for hands-free clapping.", 14, 0, 100, 0, 0, 0, "Ace", 37345);
+
+DELETE FROM creature_text WHERE entry=39456;
+INSERT INTO creature_text VALUES 
+(39456, 0, 0, "Um...you do realise there's a bottom to this cage,no?!", 12, 0, 100, 0, 0, 0, "Captured Goblin", 39496),
+(39456, 1, 0, "Mommy!", 12, 0, 100, 0, 0, 0, "Captured Goblin", 39497),
+(39456, 2, 0, "3... 2... 1...", 12, 0, 100, 0, 0, 0, "Captured Goblin", 39498),
+(39456, 3, 0, "Just open the door... oh no!", 12, 0, 100, 0, 0, 0, "Captured Goblin", 39500),
+(39456, 4, 0, "What are those rockets for?!", 12, 0, 100, 0, 0, 0, "Captured Goblin", 39501),
+(39456, 5, 0, "$N, you and Hobart are going to be the death of me!", 12, 0, 100, 0, 0, 0, "Captured Goblin", 39502),
+(39456, 6, 0, "The pirates have keys!", 12, 0, 100, 0, 0, 0, "Captured Goblin", 39503);
+
+DELETE FROM creature_text WHERE entry=39426;
+INSERT INTO creature_text VALUES 
+(39426, 0, 0, "But, sweetie...?", 12, 0, 100, 0, 0, 0, "Candy Cane", 39465),
+(39426, 1, 0, "You always secretly thought I looked fat!", 14, 0, 100, 0, 0, 0, "Candy Cane", 40619);
+
+DELETE FROM creature_text WHERE entry=39582;
+INSERT INTO creature_text VALUES 
+(39582, 0, 0, "I SEE THE TRAITOR IS HERE TO RESCUE YOU, WARCHIEF. HOW CONVENIENT. YOU WILL BOTH BOW TO ME OR FALL TOGETHER!", 14, 0, 100, 0, 0, 19567, "Trade Prince Gallywix", 39530),
+(39582, 1, 0, "I'm beaten. You've shown me the error of my ways. From here on out, I promise to reform the way the cartel is run!", 14, 0, 100, 0, 0, 19581, "Trade Prince Gallywix", 39536),
+(39582, 2, 0, "Uncle! Uncle! I give! You guys are too much for me!", 14, 0, 100, 0, 0, 19580, "Trade Prince Gallywix", 39542),
+(39582, 3, 0, "I'm your goblin, Thrall. What would you have of me?!", 14, 0, 100, 0, 0, 19582, "Trade Prince Gallywix", 39543),
+(39582, 4, 0, "Here, I need to unload some toxic assets!", 14, 0, 100, 0, 0, 19568, "Trade Prince Gallywix", 39551),
+(39582, 5, 0, "I'm all fired up over finally gettin' rid of you!", 14, 0, 100, 0, 0, 19575, "Trade Prince Gallywix", 39563),
+(39582, 6, 0, "You burned down my headquarters. Now I'm gonna burn down you!", 14, 0, 100, 0, 0, 19576, "Trade Prince Gallywix", 39564),
+(39582, 7, 0, "Here's some of that moolah back that I took from you!", 14, 0, 100, 0, 0, 19577, "Trade Prince Gallywix", 39572),
+(39582, 8, 0, "I like you. Here's a raise!", 14, 0, 100, 0, 0, 19578, "Trade Prince Gallywix", 39573),
+(39582, 9, 0, "It will be as you say! Long live the Bilgewater Cartel! For the Horde!", 14, 0, 100, 0, 0, 19583, "Trade Prince Gallywix", 39589);
+
+
