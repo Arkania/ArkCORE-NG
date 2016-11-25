8c28808d6db7d78d114e8261bb976423ae70ab5a
gpn39f <frankberg@foni.net>
2016-11-25 16:03:51 +0100

    fix issue #200
    insert npc 39117
    correct order in RequiredNpcOrGo and count..

---
+++ sql/updates/world/2016_11_15_06_world.sql
@@ -0,0 +1,8 @@
+
+DELETE FROM creature WHERE guid=2002;
+INSERT INTO creature VALUES 
+(2002, 39117, 0, 0, 0, 1, "169", 0, 31313, 0, 2242.28, 230.375, 34.5098, 0.383972, 90, 0, 0, 404, 456, 0, 0, 0, 0);
+
+UPDATE quest_template SET RequiredNpcOrGo1=1675, RequiredNpcOrGo3=0, RequiredNpcOrGoCount1=5, RequiredNpcOrGoCount3=0, RequiredItemId1=2834, RequiredItemId4=0, RequiredItemCount1=6, RequiredItemCount4=0 WHERE Id=24997;
+
+
