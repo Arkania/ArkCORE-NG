a2b59d6e394b0bab3194b59077186217b82fd9eb
gpn39f <frankberg@foni.net>
2017-02-10 16:55:37 +0100

    fix wrong handled random movement inside water.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -3176,7 +3176,7 @@ bool Unit::IsUnderWater() const
 
 void Unit::UpdateUnderwaterState(Map* m, float x, float y, float z)
 {
-    if (!IsPet() && !IsVehicle())
+    if (IsFlying() || (!IsPet() && !IsVehicle()))
         return;
 
     LiquidData liquid_status;
