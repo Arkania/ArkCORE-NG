195ac7c5436a439d333942b3448b333099c094a6
Björn Malz <Bjrn Malz>
2017-02-17 18:40:18 +0100

    Fix for command gobject set phase Include command gobject set phasegroup

---
+++ sql/updates/world/2017_02_17_01_world.sql
@@ -0,0 +1,4 @@
+
+
+INSERT INTO `command` (`name`, `permission`, `help`) VALUES
+('gobject set phasegroup', 397, 'Syntax: .gobject set phasegroup #guid #phasegroup\r\n\r\nGameobject with DB guid #guid phasegroup changed to #phasegroupwith related world vision update for players. Gameobject state saved to DB and persistent.');
