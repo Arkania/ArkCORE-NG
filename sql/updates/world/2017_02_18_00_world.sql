32b1840a0f6dfe127caa62671befcf4d3f7f6fc2
gpn39f <frankberg@foni.net>
2017-02-18 07:33:28 +0100

    fix eye_of_acherus, fill spell in actionbar

---
+++ sql/updates/world/2017_02_18_00_world.sql
@@ -0,0 +1,7 @@
+
+DELETE FROM creature_text WHERE entry=28511;
+INSERT INTO creature_text VALUES 
+(28511, 0, 0, "The Eye of Acherus launches towards its destination.", 42, 0, 100, 0, 0, 0, "Eye of Acherus", 28507),
+(28511, 1, 0, "The Eye of Acherus is in your control.", 42, 0, 100, 0, 0, 0, "Eye of Acherus", 28465);
+
+
