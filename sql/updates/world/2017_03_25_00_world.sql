776274f568ca706a362056418b364bf33baed0cf
gpn39f <frankberg@foni.net>
2017-03-31 23:39:50 +0200

    fix for Aliance quest=27239
    thanks to roger

---
+++ sql/updates/world/2017_03_25_00_world.sql
@@ -0,0 +1,5 @@
+
+-- fix for Aliance quest=27239
+
+UPDATE `quest_template` SET `Method`=2,`Flags`=16394,`SpecialFlags`=2 WHERE `Id`=27239;
+
--- src/server/scripts/World/npc_taxi.cpp
+++ src/server/scripts/World/npc_taxi.cpp
@@ -166,7 +166,7 @@ public:
             player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_TORASTRASZA2, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 22);
             break;
         case 23704: // Dustwallow Marsh - Cassa Crimsonwing
-            if (player->GetQuestStatus(11142) == QUEST_STATUS_INCOMPLETE)
+            if (player->GetQuestStatus(26257) == QUEST_STATUS_INCOMPLETE)
                 player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CRIMSONWING, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+25);
             break;
         case 17209:
