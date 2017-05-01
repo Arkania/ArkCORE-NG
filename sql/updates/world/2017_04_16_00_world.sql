6067dafec6def21b6e514d2153345c4b9e937247
gpn39f <frankberg@foni.net>
2017-05-01 19:50:09 +0200

    Cleanup april sql update contend

--- sql/updates/world/2017_04_16_00_world.sql
+++
@@ -1,7 +0,0 @@
-
--- Misc fixes for Tinker Town
-UPDATE `creature_template` SET `minlevel`=3,`maxlevel`=4 WHERE `entry`=43230;
-UPDATE `creature_template` SET `minlevel`=3,`maxlevel`=4 WHERE `entry`=42223;
-UPDATE `creature_template_addon` SET `emote`=214 WHERE `entry`=43230;
-UPDATE `creature_equip_template` SET `itemEntry1`=49711,`itemEntry2`=0,`itemEntry3`=0 WHERE `entry`=43230;
-
--- sql/updates/world/2017_04_16_02_world_434.sql
+++
@@ -1,7 +0,0 @@
-
-ALTER TABLE `creature_addon`
-ADD COLUMN `aiAnimKit`  smallint(6) NOT NULL DEFAULT 0 AFTER `emote`,
-ADD COLUMN `movementAnimKit`  smallint(6) NOT NULL DEFAULT 0 AFTER `aiAnimKit`,
-ADD COLUMN `meleeAnimKit`  smallint(6) NOT NULL DEFAULT 0 AFTER `movementAnimKit`;
-
-
