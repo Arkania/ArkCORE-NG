034a22b4c8c14b435f4e04b7bafd9ac8e86ee5bd
gpn39f <frankberg@foni.net>
2017-06-15 18:17:20 +0200

    sorry .. remove more wrong content from sniff..

--- sql/updates/world/2017_06_13_00_world.sql
+++ sql/updates/world/2017_06_13_00_world.sql
@@ -1,54 +1,16 @@
 
 
-
 DELETE FROM creature_template_addon WHERE `entry` IN (62922 /*62922 (Crimson Shale Hatchling)*/, 62181 /*62181 (Topaz Shale Hatchling)*/, 62916 /*62916 (Fungal Moth)*/, 62118 /*62118 (Twilight Beetle)*/, 62117 /*62117 (Twilight Spider)*/, 62915 /*62915 (Emerald Shale Hatchling)*/, 62182 /*62182 (Amethyst Shale Hatchling)*/, 62925 /*62925 (Crystal Beetle)*/, 66804 /*66804 (Crystallus)*/, 66805 /*66805 (Ruby)*/, 66802 /*66802 (Fracture)*/, 66815 /*66815 (Bordin Steadyfist)*/, 62924 /*62924 (Deepholm Cockroach)*/);
-INSERT INTO creature_template_addon (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
-(62922, 0, 0, 0, 1, 0, ''), -- 62922 (Crimson Shale Hatchling)
-(62181, 0, 0, 0, 1, 0, ''), -- 62181 (Topaz Shale Hatchling)
-(62916, 0, 0, 50331648, 1, 0, ''), -- 62916 (Fungal Moth)
-(62118, 0, 0, 0, 1, 0, ''), -- 62118 (Twilight Beetle)
-(62117, 0, 0, 0, 1, 0, ''), -- 62117 (Twilight Spider)
-(62915, 0, 0, 0, 1, 0, ''), -- 62915 (Emerald Shale Hatchling)
-(62182, 0, 0, 0, 1, 0, ''), -- 62182 (Amethyst Shale Hatchling)
-(62925, 0, 0, 0, 1, 0, ''), -- 62925 (Crystal Beetle)
-(66804, 0, 0, 0, 1, 0, ''), -- 66804 (Crystallus)
-(66805, 0, 0, 0, 1, 0, ''), -- 66805 (Ruby)
-(66802, 0, 0, 0, 1, 0, ''), -- 66802 (Fracture)
-(66815, 0, 0, 0, 257, 0, ''), -- 66815 (Bordin Steadyfist)
-(62924, 0, 0, 0, 1, 0, ''); -- 62924 (Deepholm Cockroach)
 
 DELETE FROM creature_template_addon WHERE `entry` IN (66815 /*66815 (Bordin Steadyfist)*/, 66802 /*66802 (Fracture)*/, 66804 /*66804 (Crystallus)*/, 66805 /*66805 (Ruby)*/, 62915 /*62915 (Emerald Shale Hatchling)*/, 62916 /*62916 (Fungal Moth)*/, 62924 /*62924 (Deepholm Cockroach)*/, 62925 /*62925 (Crystal Beetle)*/);
-INSERT INTO creature_template_addon (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
-(66815, 0, 0, 0, 257, 0, ''), -- 66815 (Bordin Steadyfist)
-(66802, 0, 0, 0, 1, 0, ''), -- 66802 (Fracture)
-(66804, 0, 0, 0, 1, 0, ''), -- 66804 (Crystallus)
-(66805, 0, 0, 0, 1, 0, ''), -- 66805 (Ruby)
-(62915, 0, 0, 0, 1, 0, ''), -- 62915 (Emerald Shale Hatchling)
-(62916, 0, 0, 50331648, 1, 0, ''), -- 62916 (Fungal Moth)
-(62924, 0, 0, 0, 1, 0, ''), -- 62924 (Deepholm Cockroach)
-(62925, 0, 0, 0, 1, 0, ''); -- 62925 (Crystal Beetle)
 
 DELETE FROM creature_template_addon WHERE `entry` IN (66802 /*66802 (Fracture)*/, 66815 /*66815 (Bordin Steadyfist)*/, 66805 /*66805 (Ruby)*/, 66804 /*66804 (Crystallus)*/, 62117 /*62117 (Twilight Spider)*/, 62181 /*62181 (Topaz Shale Hatchling)*/, 62182 /*62182 (Amethyst Shale Hatchling)*/, 62927 /*62927 (Crimson Geode)*/, 62922 /*62922 (Crimson Shale Hatchling)*/, 62924 /*62924 (Deepholm Cockroach)*/, 62915 /*62915 (Emerald Shale Hatchling)*/, 62118 /*62118 (Twilight Beetle)*/, 62925 /*62925 (Crystal Beetle)*/);
-INSERT INTO creature_template_addon (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
-(66802, 0, 0, 0, 1, 0, ''), -- 66802 (Fracture)
-(66815, 0, 0, 0, 257, 0, ''), -- 66815 (Bordin Steadyfist)
-(66805, 0, 0, 0, 1, 0, ''), -- 66805 (Ruby)
-(66804, 0, 0, 0, 1, 0, ''), -- 66804 (Crystallus)
-(62117, 0, 0, 0, 1, 0, ''), -- 62117 (Twilight Spider)
-(62181, 0, 0, 0, 1, 0, ''), -- 62181 (Topaz Shale Hatchling)
-(62182, 0, 0, 0, 1, 0, ''), -- 62182 (Amethyst Shale Hatchling)
-(62927, 0, 0, 33554432, 1, 0, ''), -- 62927 (Crimson Geode)
-(62922, 0, 0, 0, 1, 0, ''), -- 62922 (Crimson Shale Hatchling)
-(62924, 0, 0, 0, 1, 0, ''), -- 62924 (Deepholm Cockroach)
-(62915, 0, 0, 0, 1, 0, ''), -- 62915 (Emerald Shale Hatchling)
-(62118, 0, 0, 0, 1, 0, ''), -- 62118 (Twilight Beetle)
-(62925, 0, 0, 0, 1, 0, ''); -- 62925 (Crystal Beetle)
 
 UPDATE creature_template_addon SET `auras`='36032' WHERE `entry`=44619; -- Arcane Charge
 UPDATE creature_template_addon SET `auras`='79892 82571 82526' WHERE `entry`=43170; -- Cosmetic - Shaman Rocks
 UPDATE creature_template_addon SET `auras`='79849 83810 83811' WHERE `entry`=44855; -- Molten Armor -- Cosmetic - Fire Channel -- Cosmetic - Water Channel
-UPDATE creature_template_addon SET `auras`='61424' WHERE `entry`=32638; -- Traveler's Tundra Mammoth
-UPDATE creature_template_addon SET `auras`='61424' WHERE `entry`=32639; -- Traveler's Tundra Mammoth
+UPDATE creature_template_addon SET `auras`='' WHERE `entry`=32638; -- Traveler's Tundra Mammoth
+UPDATE creature_template_addon SET `auras`='' WHERE `entry`=32639; -- Traveler's Tundra Mammoth
 UPDATE creature_template_addon SET `bytes1`=262144, `auras`='29266 28559' WHERE `entry`=45364; -- Permanent Feign Death, Untrackable
 UPDATE creature_template_addon SET `auras`='100803' WHERE `entry`=53739; -- Impetuous
 UPDATE creature_template_addon SET `bytes1`=50331648 WHERE `entry`=43254; -- Wild Energy Passive
@@ -65,7 +27,7 @@ UPDATE creature_template_addon SET `auras`='79949' WHERE `entry`=42731; -- Water
 UPDATE creature_template_addon SET `auras`='78148 78150 83458' WHERE `entry`=44350; -- Windfury, Flametongue Weapon
 UPDATE creature_template_addon SET `auras`='79949 83458' WHERE `entry`=44351; -- Water Shield -- No Mana Cost, Mana Regen
 UPDATE creature_template_addon SET `bytes1`=65536, `auras`='79977 79976 79972' WHERE `entry`=43232; -- Blessing of Might,, Retribution Aura, Seal of Truth
-UPDATE creature_template_addon SET `mount`=45448, `auras`='29266' WHERE `entry`=43250; -- Permanent Feign Death
+UPDATE creature_template_addon SET `mount`=8797, `auras`='29266' WHERE `entry`=43250; -- Permanent Feign Death
 UPDATE creature_template_addon SET `auras`='29266' WHERE `entry`=43134; -- Permanent Feign Death
 UPDATE creature_template_addon SET `auras`='29266' WHERE `entry`=43174; -- Permanent Feign Death
 UPDATE creature_template_addon SET `auras`='83458 79949' WHERE `entry`=44673; -- No Mana Cost, Mana Regen, Water Shield
