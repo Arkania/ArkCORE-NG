2ddf3ae9e90159b08b5618802c06b72b9c8bcd31
gpn39f <frankberg@foni.net>
2017-09-02 01:30:00 +0200

    akke report some crash.. i need more sniff for this.. so i can only insert a hack to prevent crash..

---
+++ sql/updates/world/2017_09_02_00_world.sql
@@ -0,0 +1,17 @@
+
+
+delete from game_event_creature where eventEntry=1 and guid=134629;
+insert into game_event_creature values 
+(1, 134629);
+
+UPDATE quest_template SET Method=2, Type=82, Flags=65536, SpecialFlags=2 WHERE Id=28927;
+
+UPDATE quest_template SET Method=2, Type=82, Flags=4, SpecialFlags=2 WHERE Id=28914;
+
+delete from gameobject_queststarter where id=207986 and quest= 28914;
+
+update creature set phaseId=170 where guid=99470 and id=45272;
+
+update creature set phaseId=170 where guid=99468 and id=41458;
+	
+	
