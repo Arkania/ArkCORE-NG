68ccc1162b5741f2cbc10f47f047e3234792c9b0
gpn39f <frankberg@foni.net>
2017-09-14 02:06:11 +0200

    cleanup sql update folder
    remove vashjir updates from last month to import all in new vashjir develop folder

--- sql/updates/world/2017_09_04_01_world.sql
+++
@@ -1,8 +0,0 @@
-
--- set respawn for nazgrim and vanGuard to short respawn value, so each loop of ship to vashir can take player..
-update creature set spawntimesecs=45 where id in (43090, 43100);
-
-update creature set spawntimesecs=45 where id in (42021, 42022, 42095, 42103);
-
-
-
--- sql/updates/world/2017_09_05_02_world.sql
+++ sql/updates/world/2017_09_05_02_world.sql
@@ -1,36 +1,4 @@
 
+-- Stormfury Revenant (Normal)
 UPDATE quest_template SET QuestGiverPortrait=0 WHERE Id=28826;
 
-delete from creature where guid in (26874,51722,51749,51750,51751,51752,51753,51754,51755,51756,51757,51758, 49430);
-
-delete from creature_addon where guid in (26874,51722,51749,51750,51751,51752,51753,51754,51755,51756,51757,51758, 49430);
-
-update creature set phaseId=169, phaseGroup=0 where guid in (1112, 1176, 48894, 49026, 49800);
-
-update gameobject set phaseId=0, phaseGroup=368 where id in (208437, 208438);
-
-update creature_addon set bytes1=33554434, bytes2=1 where guid in (500032, 500033, 500036); 
-
-update creature_addon set bytes1=33554433, bytes2=1 where guid in (500034, 500035, 500037); 
-
-delete from creature where id=52654 and guid=53432;
-
-update creature_template set faction=35 where entry=52654;
-
-update creature_template set InhabitType=4 where entry=52806;
-
-update transports set phaseId=171 where guid=40;  -- 203466
-
-update transports set phaseId=171 where guid=42;  -- 197195
-
-update creature set phaseId=171 where map=738; -- horde ship to vashj'ir
-
-update creature set phaseId=171 where map=674; -- alliance ship to vashj'ir
-
-update creature set modelid=21342, phaseId=171, phaseGroup=0 where id=32520 and guid=49800;
-
-update creature set modelid=21342, phaseId=171, phaseGroup=0 where id=32520 and guid=43007;
-
-UPDATE creature_template SET ScriptName="npc_captain_taylor_42103" WHERE entry=42103;
-
-UPDATE creature_template SET ScriptName="npc_stormwind_soldier_42021" WHERE entry=42021;
