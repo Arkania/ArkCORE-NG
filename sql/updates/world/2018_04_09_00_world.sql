b27cb5536563b8fd09de6d5b296d23d9e6dc4485
Snuffleclaws <dragon.starglazer@gmail.com>
2018-04-09 05:44:11 -0400

    fix quest 27995 - Ruumbo demands justice

---
+++ sql/updates/world/2018_04_09_00_world.sql
@@ -0,0 +1,2 @@
+-- Add furbolg creatures to kill count for quest 27994  (Felwood, Emerald sanctuary)
+UPDATE creature_template SET KillCredit1=47329 WHERE entry=7153 OR entry=7154 OR entry=7155 OR entry=14342;
