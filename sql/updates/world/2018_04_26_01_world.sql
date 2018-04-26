3f5b7079c3a74daf172e0b2cc5b7d15129589b24
gpn39f <frankberg56@gmail.com>
2018-04-26 23:47:15 +0200

    cleanup part of feralas map

---
+++ sql/updates/world/2018_04_26_01_world.sql
@@ -0,0 +1,25 @@
+
+DELETE FROM creature_addon WHERE guid=126070;
+DELETE FROM creature_addon WHERE guid=126099;
+DELETE FROM creature_addon WHERE guid=126191;
+DELETE FROM creature_addon WHERE guid=126231;
+DELETE FROM creature_addon WHERE guid=109823;
+DELETE FROM creature_addon WHERE guid=109838;
+DELETE FROM creature_addon WHERE guid=109863;
+DELETE FROM creature_addon WHERE guid=109873;
+DELETE FROM creature_addon WHERE guid=109915;
+DELETE FROM creature_addon WHERE guid=109933;
+DELETE FROM creature_addon WHERE guid=109966;
+DELETE FROM creature_addon WHERE guid=109974;
+DELETE FROM creature_addon WHERE guid=109981;
+DELETE FROM creature_addon WHERE guid=110019;
+DELETE FROM creature_addon WHERE guid=110063;
+DELETE FROM creature_addon WHERE guid=110091;
+DELETE FROM creature_addon WHERE guid=110107;
+DELETE FROM creature_addon WHERE guid=110118;
+DELETE FROM creature_addon WHERE guid=110145;
+DELETE FROM creature_addon WHERE guid=110150;
+DELETE FROM creature_addon WHERE guid=110170;
+DELETE FROM creature_addon WHERE guid=110259;
+DELETE FROM creature_addon WHERE guid=110262;
+
