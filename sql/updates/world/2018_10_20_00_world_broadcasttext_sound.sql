b749c1b3be14ae82a234b58983f0ec34d3c05f01
gpn39f <frankberg56@gmail.com>
2018-10-20 11:21:28 +0200

    cleanup table BroadcastText
    it seems some data from 548 client or newer are stored into BroadcastText
    when the core is find such entries, its ignore the complete text.. so we delete wrong sound and language entries.

---
+++ sql/updates/world/2018_10_20_00_world_broadcasttext_sound.sql
@@ -0,0 +1,12 @@
+
+-- it seems some data from 548 client or newer are stored into BroadcastText
+-- when the core is find such entries, its ignore the complete text.. better is we delete wrong sound and language entries.
+
+-- in client 434 the SoundEntry.db2 has id's upto 26682 
+update broadcast_text set SoundId=0 where SoundId > 26682;
+
+update broadcast_text set SoundId=0 where SoundId = 547;
+
+update broadcast_text set Language=0 where Language = 42;
+
+update broadcast_text set Language=0 where Language = 168;
